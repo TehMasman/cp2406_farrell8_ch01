d1901db77afb783bd946f2427d885f99a7a6c1a2
Sam Kahsnitz <samuel.kahsnitz@my.jcu.edu.au>
2017-08-01 10:54:39 +1000

    Finished

---
+++ DebuggingExercises/MovieQuote.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jc243967 on 1/08/17.
+ */
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("I'll be back.");
+    }
+}
---
+++ DebuggingExercises/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jc243967 on 1/08/17.
+ */
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Quote - I'll be back");
+        System.out.println("Movie - The Terminator");
+        System.out.println("Character - The Terminator");
+    }
+}
